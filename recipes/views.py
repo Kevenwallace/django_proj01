ed924f53dda2cab0e64cff51ec1bc7d2638b061a
keven_wallace <kevenwallace_@hotmail.com>
2022-08-01 01:42:54 -0300

    global templates

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-views.html')
+
+
+def dale(request):
+    return render(request, 'global/home.html')
