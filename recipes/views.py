2b80a129adb0f09f1a8d514298d3c9cf1a0e308f
keven_wallace <kevenwallace_@hotmail.com>
2022-08-23 23:33:25 -0300

    make migrations

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
+from ultis.recipes.factory import make_recipe
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-views.html')
-
-
-def dale(request):
-    return render(request, 'global/home.html')
+    return render(request, 'recipes/pages/recipe-views.html', context={
+        'recipes': [make_recipe() for _ in range(1)],
+        "list_details": True,
+    })
