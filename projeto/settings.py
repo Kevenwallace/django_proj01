398081325cdd4690da96e5c55456303b733af879
keven_wallace <kevenwallace_@hotmail.com>
2022-12-07 18:45:55 -0300

    css

--- projeto/settings.py
+++ projeto/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(SECRET_KEY, "insecure")
+SECRET_KEY = os.environ.get('SECRET_KEY', "insecure")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes',
+    'authors',
 ]
 
 MIDDLEWARE = [
